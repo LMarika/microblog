ede302654ebddec3f4e97c601cbdd6ee099eb525
LMarika <mari.fm.ry@yandex.ru>
2025-04-07 11:50:34 +0300

    Added page-flipping functionality

--- config.py
+++ config.py
@@ -9,4 +9,5 @@ class Config:
   MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-  ADMINS = ['LpMarika@yandex.ru']
+  ADMINS = ['LMarika']
+  POSTS_PER_PAGE = 10
